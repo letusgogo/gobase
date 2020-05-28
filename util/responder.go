823929cff4c93fc62fa81eabeb4958a52b89d903
于海洋 <yhy@iothinking.com>
2020-05-28 17:11:23 +0800

    增加了 CopyErrInfo 从定义好的函数

--- util/responder.go
+++ util/responder.go
@@ -19,8 +19,10 @@ type ErrInfo struct {
 }
 
 // 从定义好的 ErrInfo 构造错误对象。可以替换掉里面的 Msg
-// NewErrInfo(ErrDataBase).Msg = "redis error"
-func NewErrInfo(err *ErrInfo) *ErrInfo {
+// errInfo := CopyErrInfo(ErrDataBase)
+// errInfo.Msg = "redis error"
+
+func CopyErrInfo(err *ErrInfo) *ErrInfo {
 	return &ErrInfo{Ret: err.Ret, Msg: err.Msg, Error: err.Error}
 }
 
