78b89939597034a16efd40f21fcb45415cb4de09
于海洋 <yhy@iothinking.com>
2020-06-10 15:59:52 +0800

    增加了 GetOffset() 方法

--- util/page.go
+++ util/page.go
@@ -1,19 +1,24 @@
 package util
 
 type Pageable interface {
-	GetPageNo() int32
-	GetPageSize() int32
+	GetPageNo() int64
+	GetPageSize() int64
+	GetOffset() int64
 }
 
 type DefaultPage struct {
 	pageNo,
-	pageSize int32
+	pageSize int64
 }
 
-func (p *DefaultPage) GetPageNo() int32 {
+func (p *DefaultPage) GetOffset() int64 {
+	return (p.pageNo - 1) * p.pageSize
+}
+
+func (p *DefaultPage) GetPageNo() int64 {
 	return p.pageNo
 }
 
-func (p *DefaultPage) GetPageSize() int32 {
+func (p *DefaultPage) GetPageSize() int64 {
 	return p.pageSize
 }
