dd9e0f1c1d65416004cd83123f0ee99c825168b9
于海洋 <yhy@iothinking.com>
2020-06-22 14:49:32 +0800

    增加了 NewErrInfo 从一个已存在的全局 ErrInfo 创建一个新的 ErrInfo

--- util/responder.go
+++ util/responder.go
@@ -27,13 +27,34 @@ func (e *ErrInfo) GetMsg() string {
 }
 
 //ret=0 成功。
+//noinspection ALL
 var (
 	ErrNot    = &ErrInfo{0, "success", nil}
-	ErrUnknow = &ErrInfo{1, "unknow error", nil}
+	ErrUnknow = &ErrInfo{1, "unknown error", nil}
 
 	ErrParam    = &ErrInfo{2, "param error", nil}
 	ErrDataBase = &ErrInfo{3, "database error", nil}
 )
+
+// 从 info 创建一个新的 ErrInfo 类型的对象。
+// 当 msg 不为空,则用 msg 替换原 msg
+// 当 err 不为 nil,则用 err 替换 原 err
+//noinspection ALL
+func NewErrInfo(info *ErrInfo, msg string, err error) *ErrInfo {
+	errInfo := &ErrInfo{
+		Ret:   info.Ret,
+		Msg:   info.Msg,
+		Error: info.Error,
+	}
+	if msg != "" {
+		errInfo.Msg = msg
+	}
+
+	if err != nil {
+		errInfo.Error = err
+	}
+	return errInfo
+}
 
 func WriteRpcRsp(rspPtr interface{}, rpcError RpcError, data map[string]interface{}) {
 	if nil == data {
@@ -47,6 +68,7 @@ func WriteRpcRsp(rspPtr interface{}, rpcError RpcError, data map[string]interfac
 	SetStructVals(rspPtr, data)
 }
 
+//noinspection ALL
 func WriteRpcRspWithMsg(rspPtr interface{}, rpcError RpcError, msg string, data map[string]interface{}) {
 	if nil == data {
 		data = make(map[string]interface{})
