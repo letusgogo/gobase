56a164dadd8ff5957a3bba1a8a7d683f85d31437
weizhen <weizhen@iothinking.com>
2020-12-28 19:33:37 +0800

    优化加密工具类

--- util/crypto.go
+++ util/crypto.go
@@ -10,32 +10,15 @@ import (
 )
 
 ///////////////////////////加密的入口 ////////////////////////////////
-type crypto struct {
-	mode modeMap
-	pad  padMap
-	aes  aesEncryption
-	//encoding的意思
-	enc encMap
-}
-
-func NewCrypto() *crypto {
-	crypto := new(crypto)
-	crypto.mode.CBC = &cBC{}
-	crypto.pad.PKCS5 = &pKCS5{}
-	return crypto
-}
-
-type encMap struct {
+type cryptoUtil struct {
+	aes    aesEncryption
 	base64 base64Encryption
 	md5    md5Encryption
 }
 
-type padMap struct {
-	PKCS5 pad
-}
-
-type modeMap struct {
-	CBC mode
+func NewCryptoUtil() *cryptoUtil {
+	crypto := new(cryptoUtil)
+	return crypto
 }
 
 ///////////////////////////////补全方式的接口和实现////////////////////////
@@ -44,10 +27,10 @@ type pad interface {
 	UnPadding(origData []byte) []byte
 }
 
-type pKCS5 struct {
+type PKCS5 struct {
 }
 
-func (P pKCS5) Padding(ciphertext []byte, blockSize int) []byte {
+func (P PKCS5) Padding(ciphertext []byte, blockSize int) []byte {
 	padding := blockSize - len(ciphertext)%blockSize
 	//填充
 	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
@@ -55,7 +38,7 @@ func (P pKCS5) Padding(ciphertext []byte, blockSize int) []byte {
 	return append(ciphertext, padtext...)
 }
 
-func (P pKCS5) UnPadding(origData []byte) []byte {
+func (P PKCS5) UnPadding(origData []byte) []byte {
 	length := len(origData)
 	unPadding := int(origData[length-1])
 	return origData[:(length - unPadding)]
@@ -63,81 +46,92 @@ func (P pKCS5) UnPadding(origData []byte) []byte {
 
 /////////////////////////////加密方式的接口和实现///////////////////////
 type mode interface {
-	Encrypt(iv []byte, block cipher.Block, encodeStr []byte) ([]byte, error)
-	Decrypt(iv []byte, block cipher.Block, encodeStr []byte) ([]byte, error)
+	Encrypt(iv []byte, block cipher.Block, encodeBytes []byte) ([]byte, error)
+	Decrypt(iv []byte, block cipher.Block, decodeBytes []byte) ([]byte, error)
 }
 
-type cBC struct {
+type CBC struct {
 }
 
-func (cbc *cBC) Encrypt(iv []byte, block cipher.Block, encodeStr []byte) ([]byte, error) {
+func (cbc *CBC) Encrypt(iv []byte, block cipher.Block, encodeBytes []byte) ([]byte, error) {
 	blockMode := cipher.NewCBCEncrypter(block, iv)
-	crypted := make([]byte, len(encodeStr))
-	blockMode.CryptBlocks(crypted, encodeStr)
+	crypted := make([]byte, len(encodeBytes))
+	blockMode.CryptBlocks(crypted, encodeBytes)
 	return crypted, nil
 }
 
-func (cbc *cBC) Decrypt(iv []byte, block cipher.Block, decodeStr []byte) ([]byte, error) {
+func (cbc *CBC) Decrypt(iv []byte, block cipher.Block, decodeBytes []byte) ([]byte, error) {
 	blockMode := cipher.NewCBCDecrypter(block, iv)
-	origData := make([]byte, len(decodeStr))
-	blockMode.CryptBlocks(origData, decodeStr)
+	origData := make([]byte, len(decodeBytes))
+	blockMode.CryptBlocks(origData, decodeBytes)
 	return origData, nil
 }
 
 //////////////////////////加密的接口和实现类//////////////////////////////
 type Encryption interface {
-	Encrypt(encodeStr []byte) ([]byte, error)
-	Decrypt(decodeStr []byte) ([]byte, error)
+	Encrypt(encodeBytes []byte) ([]byte, error)
+	Decrypt(decodeBytes []byte) ([]byte, error)
 }
 
 //=======================Aes实现===========================
 type aesEncryption struct {
 	//key
-	secretKey string
+	secretKey []byte
 	//iv
-	secretIv string
+	secretIv []byte
 	//加密方式
 	mode mode
 	//补全方式
 	pad pad
 }
 
-func NewAesEncryption(secretKey string, secretIv string) *aesEncryption {
-	return &aesEncryption{secretKey: secretKey, secretIv: secretIv}
-}
-
-func (aesEncryption *aesEncryption) setOption(SecretKey []byte, SecretIv []byte, mode mode, pad pad) error {
+func (aesEncryption *aesEncryption) judgeOption() error {
+	SecretKey := aesEncryption.secretKey
+	SecretIv := aesEncryption.secretIv
 	if len(SecretKey) != 16 && len(SecretKey) != 24 && len(SecretKey) != 32 {
-		return errors.New("")
+		return errors.New("key length must be 16 or 24 or 32 byte")
 	}
 	if len(SecretKey) != len(SecretIv) {
-		return errors.New("")
+		return errors.New("key and iv must be equal")
 	}
-	aesEncryption.secretKey = string(SecretKey)
-	aesEncryption.secretIv = string(SecretIv)
-	aesEncryption.mode = mode
-	aesEncryption.pad = pad
+	if aesEncryption.mode == nil || aesEncryption.pad == nil {
+		return errors.New("mode and pad not empty")
+	}
 	return nil
 }
 
-func (aesEncryption *aesEncryption) Encrypt(encodeStr []byte) ([]byte, error) {
+func (aesEncryption *aesEncryption) SetOption(SecretKey []byte, SecretIv []byte, mode mode, pad pad) error {
+	aesEncryption.secretKey = SecretKey
+	aesEncryption.secretIv = SecretIv
+	aesEncryption.mode = mode
+	aesEncryption.pad = pad
+	return aesEncryption.judgeOption()
+}
+
+func (aesEncryption *aesEncryption) Encrypt(encodeBytes []byte) ([]byte, error) {
+	if err := aesEncryption.judgeOption(); err != nil {
+		return nil, err
+	}
 	//根据key 生成密文
-	block, err := aes.NewCipher([]byte(aesEncryption.secretKey))
+	block, err := aes.NewCipher(aesEncryption.secretKey)
 	if err != nil {
 		return nil, err
 	}
 	blockSize := block.BlockSize()
-	encodeStr = aesEncryption.pad.Padding(encodeStr, blockSize)
-	crypted, _ := aesEncryption.mode.Encrypt([]byte(aesEncryption.secretIv), block, encodeStr)
+	encodeBytes = aesEncryption.pad.Padding(encodeBytes, blockSize)
+	crypted, _ := aesEncryption.mode.Encrypt(aesEncryption.secretIv, block, encodeBytes)
 	return crypted, nil
 }
 
-func (aesEncryption *aesEncryption) Decrypt(decodeStr []byte) ([]byte, error) {
-	block, err := aes.NewCipher([]byte(aesEncryption.secretKey))
+func (aesEncryption *aesEncryption) Decrypt(decodeBytes []byte) ([]byte, error) {
+	if err := aesEncryption.judgeOption(); err != nil {
+		return nil, err
+	}
+	block, err := aes.NewCipher(aesEncryption.secretKey)
 	if err != nil {
 		return nil, err
 	}
-	origData, _ := aesEncryption.mode.Decrypt([]byte(aesEncryption.secretIv), block, decodeStr)
+	origData, _ := aesEncryption.mode.Decrypt(aesEncryption.secretIv, block, decodeBytes)
 	origData = aesEncryption.pad.UnPadding(origData)
 	return origData, nil
 }
@@ -146,14 +140,14 @@ func (aesEncryption *aesEncryption) Decrypt(decodeStr []byte) ([]byte, error) {
 type md5Encryption struct {
 }
 
-func (md5Encryption md5Encryption) Encrypt(encodeStr []byte) ([]byte, error) {
+func (md5Encryption md5Encryption) Encrypt(encodeBytes []byte) ([]byte, error) {
 	h := md5.New()
-	h.Write(encodeStr)
+	h.Write(encodeBytes)
 	return h.Sum(nil), nil
 	//return hex.EncodeToString(h.Sum(nil)), nil
 }
 
-func (md5Encryption md5Encryption) Decrypt(decodeStr []byte) ([]byte, error) {
+func (md5Encryption md5Encryption) Decrypt(decodeBytes []byte) ([]byte, error) {
 	panic("please implement me")
 }
 
@@ -161,12 +155,12 @@ func (md5Encryption md5Encryption) Decrypt(decodeStr []byte) ([]byte, error) {
 type base64Encryption struct {
 }
 
-func (base64Encryption base64Encryption) Encrypt(encodeStr []byte) ([]byte, error) {
-	return []byte(base64.StdEncoding.EncodeToString(encodeStr)), nil
+func (base64Encryption base64Encryption) Encrypt(encodeBytes []byte) ([]byte, error) {
+	return []byte(base64.StdEncoding.EncodeToString(encodeBytes)), nil
 }
 
-func (base64Encryption base64Encryption) Decrypt(decodeStr []byte) ([]byte, error) {
-	decodeBytes, err := base64.StdEncoding.DecodeString(string(decodeStr))
+func (base64Encryption base64Encryption) Decrypt(decodeBytes []byte) ([]byte, error) {
+	decodeBytes, err := base64.StdEncoding.DecodeString(string(decodeBytes))
 	if err != nil {
 		return nil, err
 	}
--- util/crypto_test.go
+++ util/crypto_test.go
@@ -5,31 +5,17 @@ import (
 )
 
 func Test(t *testing.T) {
-	aesCBCEncryption := NewAesEncryption("1234567891234567", "1234567891234567")
-	DecryptStr, _ := aesCBCEncryption.Encrypt([]byte("1234567891234567"))
-	t.Log("aes加密=" + string(DecryptStr))
-	EncryptStr, _ := aesCBCEncryption.Decrypt(DecryptStr)
-	t.Log("aes解密=" + string(EncryptStr))
-
-	base64Encrypt, _ := base64Encryption{}.Encrypt(DecryptStr)
-	t.Log("base64加密=" + string(base64Encrypt))
-	Base64Decrypt, _ := base64Encryption{}.Decrypt(base64Encrypt)
-	t.Log("base64解密=" + string(Base64Decrypt))
-	EncryptStr1, _ := aesCBCEncryption.Decrypt(Base64Decrypt)
-	t.Log("aes解密=" + string(EncryptStr1))
-
-}
-func Test1(t *testing.T) {
-	crypto := NewCrypto()
-	crypto.aes.setOption([]byte("1234567891234567"), []byte("1234567891234567"), crypto.mode.CBC, crypto.pad.PKCS5)
-	DecryptStr, _ := crypto.aes.Encrypt([]byte("1234567891234567"))
+	crypto := NewCryptoUtil()
+	crypto.aes.SetOption([]byte("1234567891234567"), []byte("1234567891234567"), new(CBC), new(PKCS5))
+	DecryptStr, err := crypto.aes.Encrypt([]byte("1234567891234567"))
+	t.Log(err)
 	t.Log("aes加密=" + string(DecryptStr))
 	EncryptStr, _ := crypto.aes.Decrypt(DecryptStr)
 	t.Log("aes解密=" + string(EncryptStr))
 
-	base64Encrypt, _ := crypto.enc.base64.Encrypt(DecryptStr)
+	base64Encrypt, _ := crypto.base64.Encrypt(DecryptStr)
 	t.Log("base64加密=" + string(base64Encrypt))
-	Base64Decrypt, _ := crypto.enc.base64.Decrypt(base64Encrypt)
+	Base64Decrypt, _ := crypto.base64.Decrypt(base64Encrypt)
 	t.Log("base64解密=" + string(Base64Decrypt))
 	EncryptStr1, _ := crypto.aes.Decrypt(Base64Decrypt)
 	t.Log("aes解密=" + string(EncryptStr1))
