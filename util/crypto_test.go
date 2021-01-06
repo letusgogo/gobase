8751d0fa572205c5a54bb46e90699d2bfcfc7469
weizhen <weizhen@iothinking.com>
2021-01-06 14:56:11 +0800

    修改了加密工具方法不合理的命名 增加了md5的测试方法

--- util/crypto_test.go
+++ util/crypto_test.go
@@ -1,12 +1,13 @@
 package util
 
 import (
+	"fmt"
 	"testing"
 )
 
 func Test(t *testing.T) {
 	crypto := NewCryptoUtil()
-	crypto.Aes.SetOption([]byte("1234567891234567"), []byte("1234567891234567"), new(CBC), new(PKCS7))
+	_ = crypto.Aes.SetOption([]byte("1234567891234567"), []byte("1234567891234567"), new(CBC), new(PKCS7))
 	DecryptStr, err := crypto.Aes.Encrypt([]byte("1234567891234567"))
 	t.Log(err)
 	t.Log("aes加密=" + string(DecryptStr))
@@ -19,4 +20,10 @@ func Test(t *testing.T) {
 	t.Log("base64解密=" + string(Base64Decrypt))
 	EncryptStr1, _ := crypto.Aes.Decrypt(Base64Decrypt)
 	t.Log("aes解密=" + string(EncryptStr1))
+}
+
+func Test_Md5(t *testing.T) {
+	crypto := NewCryptoUtil()
+	_, err := crypto.Base64.Decrypt([]byte("123456"))
+	fmt.Println(err)
 }
