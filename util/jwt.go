930256ac768a7e5776e048a9c3915d01ef152725
looper <ldw2looper@dingtalk.com>
2022-05-08 20:13:38 +0800

    jwt支持

---
+++ util/jwt.go
@@ -0,0 +1,72 @@
+package util
+
+import (
+	"errors"
+	"github.com/dgrijalva/jwt-go"
+	uuid "github.com/satori/go.uuid"
+)
+
+type JWT struct {
+	SignKey []byte
+}
+
+func NewJWT(signKey string) *JWT {
+	return &JWT{
+		[]byte(signKey),
+	}
+}
+
+type CustomClaims struct {
+	UUID        uuid.UUID
+	ID          uint
+	Username    string
+	NickName    string
+	AuthorityId string
+	BufferTime  int64
+	jwt.StandardClaims
+}
+
+var (
+	TokenExpired     = errors.New("token is expired")
+	TokenNotValidYet = errors.New("token not active yet")
+	TokenMalformed   = errors.New("that's not even a token")
+	TokenInvalid     = errors.New("couldn't handle this token")
+)
+
+// CreateToken 创建一个token
+func (j *JWT) CreateToken(claims CustomClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(j.SignKey)
+}
+
+// ParseToken 解析 token
+func (j *JWT) ParseToken(tokenString string) (*CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (i interface{}, e error) {
+		return j.SignKey, nil
+	})
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return nil, TokenMalformed
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				// Token is expired
+				return nil, TokenExpired
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				return nil, TokenNotValidYet
+			} else {
+				return nil, TokenInvalid
+			}
+		}
+	}
+	if token != nil {
+		if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+			return claims, nil
+		}
+		return nil, TokenInvalid
+
+	} else {
+		return nil, TokenInvalid
+
+	}
+
+}
