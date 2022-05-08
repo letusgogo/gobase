5c29f2405e7cd5b36e8ba4b4110bd6b136dac0ae
looper <ldw2looper@dingtalk.com>
2022-05-08 18:47:04 +0800

    追回conf

---
+++ conf/conf.go
@@ -0,0 +1,182 @@
+package conf
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+type MysqlConf struct {
+	Host         string `json:"Host"`
+	MaxIdleConns int    `json:"MaxIdleConns"`
+	MaxOpenConns int    `json:"MaxOpenConns"`
+}
+
+func (m *MysqlConf) String() string {
+	b, err := json.Marshal(*m)
+	if err != nil {
+		return fmt.Sprintf("%+v", *m)
+	}
+	var out bytes.Buffer
+	err = json.Indent(&out, b, "", "    ")
+	if err != nil {
+		return fmt.Sprintf("%+v", *m)
+	}
+	return out.String()
+}
+
+type RedisConf struct {
+	Addr        string `json:"Addr"`
+	Password    string `json:"Password"`
+	PoolSize    int    `json:"PoolSize"`
+	MaxRetries  int    `json:"MaxRetries"`
+	IdleTimeout int64  `json:"IdleTimeout"`
+}
+
+func (r *RedisConf) String() string {
+	b, err := json.Marshal(*r)
+	if err != nil {
+		return fmt.Sprintf("%+v", *r)
+	}
+	var out bytes.Buffer
+	err = json.Indent(&out, b, "", "    ")
+	if err != nil {
+		return fmt.Sprintf("%+v", *r)
+	}
+	return out.String()
+}
+
+// influxdb 配置
+type InfluxDBConf struct {
+	Addr string `json:"Addr"`
+}
+
+func (i *InfluxDBConf) String() string {
+	b, err := json.Marshal(*i)
+	if err != nil {
+		return fmt.Sprintf("%+v", *i)
+	}
+	var out bytes.Buffer
+	err = json.Indent(&out, b, "", "    ")
+	if err != nil {
+		return fmt.Sprintf("%+v", *i)
+	}
+	return out.String()
+}
+
+// 短信配置
+type SmsConf struct {
+	AccessKeyId     string `json:"AccessKeyId"`
+	AccessKeySecret string `json:"AccessKeySecret"`
+	SignName        string `json:"SignName"`
+	TemplateCode    string `json:"TemplateCode"`
+	Debug           bool   `json:"Debug"`
+}
+
+func (s *SmsConf) String() string {
+	b, err := json.Marshal(*s)
+	if err != nil {
+		return fmt.Sprintf("%+v", *s)
+	}
+	var out bytes.Buffer
+	err = json.Indent(&out, b, "", "    ")
+	if err != nil {
+		return fmt.Sprintf("%+v", *s)
+	}
+	return out.String()
+}
+
+// 配置
+type TelConf struct {
+	AccessKeyId     string `json:"AccessKeyId"`
+	AccessKeySecret string `json:"AccessKeySecret"`
+	TemplateCode    string `json:"TemplateCode"`
+	Debug           bool   `json:"Debug"`
+}
+
+func (t *TelConf) String() string {
+	b, err := json.Marshal(*t)
+	if err != nil {
+		return fmt.Sprintf("%+v", *t)
+	}
+	var out bytes.Buffer
+	err = json.Indent(&out, b, "", "    ")
+	if err != nil {
+		return fmt.Sprintf("%+v", *t)
+	}
+	return out.String()
+}
+
+// 服务的 Broker 配置
+type BrokerConf struct {
+	Type string
+	Addr string
+}
+
+func (c *BrokerConf) String() string {
+	b, err := json.Marshal(*c)
+	if err != nil {
+		return fmt.Sprintf("%+v", *c)
+	}
+	var out bytes.Buffer
+	err = json.Indent(&out, b, "", "    ")
+	if err != nil {
+		return fmt.Sprintf("%+v", *c)
+	}
+	return out.String()
+}
+
+// mqtt 配置
+type MqttConf struct {
+	Brokers []string
+}
+
+func (c *MqttConf) String() string {
+	b, err := json.Marshal(*c)
+	if err != nil {
+		return fmt.Sprintf("%+v", *c)
+	}
+	var out bytes.Buffer
+	err = json.Indent(&out, b, "", "    ")
+	if err != nil {
+		return fmt.Sprintf("%+v", *c)
+	}
+	return out.String()
+}
+
+type AppMysqlConf struct {
+	User         string `json:"user" mapstructure:"user"`
+	Pass         string `json:"pass" mapstructure:"pass"`
+	Debug        bool   `json:"debug" mapstructure:"debug"`
+	Host         string `json:"host" mapstructure:"host"`
+	DBName       string `json:"db_name" mapstructure:"db_name"`
+	MaxIdleConns int    `json:"maxIdle_conns"  mapstructure:"maxIdle_conns"`
+	MaxOpenConns int    `json:"max_open_conns" mapstructure:"max_open_conns"`
+	DBOption     string `json:"db_option" mapstructure:"db_option"`
+	LogMode      bool   `json:"log_mode" mapstructure:"log_mode"`
+}
+
+func (m *AppMysqlConf) String() string {
+	b, err := json.Marshal(*m)
+	if err != nil {
+		return fmt.Sprintf("%+v", *m)
+	}
+	var out bytes.Buffer
+	err = json.Indent(&out, b, "", "    ")
+	if err != nil {
+		return fmt.Sprintf("%+v", *m)
+	}
+	return out.String()
+}
+
+func (m *AppMysqlConf) GetMysqlUrl() string {
+	result := fmt.Sprintf("%v:%v@tcp(%v)/%v?%v", m.User, m.Pass, m.Host, m.DBName, m.DBOption)
+	return result
+}
+
+// Captcha 图形验证码
+type Captcha struct {
+	KeyLong   int32 `mapstructure:"key-long" json:"keyLong" yaml:"key-long"`
+	ImgWidth  int32 `mapstructure:"img-width" json:"imgWidth" yaml:"img-width"`
+	ImgHeight int32 `mapstructure:"img-height" json:"imgHeight" yaml:"img-height"`
+}
